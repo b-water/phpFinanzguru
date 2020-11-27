d80e2ba17958d1e93d2c79abed4467e24779c595
Nico Schmitz <nico.schmitz@jtl-software.com>
2020-11-27 15:04:53 +0100

    Fix

--- src/phpFinanzguru/Excel/Reader.php
+++ src/phpFinanzguru/Excel/Reader.php
@@ -2,8 +2,6 @@
 
 namespace bwater\phpFinanzguru\Excel;
 
-use ReaderInterface;
-
 final class Reader implements ReaderInterface {
 
     public function __construct($file) {
--- tests/ExcelReaderTest.php
+++ tests/ExcelReaderTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace bwater\phpFinanzguru\Tests;
+namespace bwater\Tests;
 
 use bwater\phpFinanzguru\Excel\Reader;
 use PHPUnit\Framework\TestCase;
